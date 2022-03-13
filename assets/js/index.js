1421b4dfe520552155298d0776ce0b910a17570b
Mitchell Frey <mfcodingbc@gmail.com>
2022-03-13 04:58:20 -0400

    save button saves necessary info to localStorage, cannot retrieve info yet + order not correct

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,17 @@
 // this variable stores an array with every hour and event
 var timeBlocks = {};
 
-// this variable selects all timeblocks(rows)
+// this variable selects all timeblocks(rows) and creates an id for each
 var timeBlocksSections = document.querySelectorAll('.row');
-    for (var i = 0; i < timeBlocksSections.length; i++)
+    for (var i = 0; i < timeBlocksSections.length; i++) {
         timeBlocksSections[i].id = 'row-' + i;
+    };
 
 // create and set ids for all hours
 var timeBlocksHourEl = document.querySelectorAll('.hour');
-    for (var i = 0; i < timeBlocksHourEl.length; i++)
+    for (var i = 0; i < timeBlocksHourEl.length; i++) {
         timeBlocksHourEl[i].id = 'hour-' + i;
+    };
 
 // create and set ids for all events per hour
 var timeBlocksEventEl = document.querySelectorAll('.col-10');
@@ -33,8 +35,72 @@ currentDayEl.textContent = currentDay;
 var currentTime = moment().format('LT');
 console.log(currentTime);
 
+var appendTimeBlock = function(timeBlockText, timeBlockList) {
+
+    var eventTextArea = $("<textarea>")
+    .addClass("col-10 lead")
+    .text(timeBlockText);
+
+    $("#event-" + timeBlockList).replaceWith(eventTextArea);
+
+    // run audit function to compare current time with time of events
+}
+
+var loadTimeBlocks = function() {
+
+    timeBlocks = JSON.parse(localStorage.getItem("timeblocks"));
+
+    if (!timeBlocks) {
+        timeBlocks = {
+            data: []
+        };
+    }
+
+console.log(timeBlocks);
+
+    $.each(timeBlocks, function(list, arr) {
+        arr.forEach(function(timeBlock) {
+            appendTimeBlock(timeBlock.text, list);
+        });
+    });
+};
+
 var saveTimeBlocks = function() {
     localStorage.setItem("timeblocks", JSON.stringify(timeBlocks));
 };
 
-// Create function that assigns ids based on position in timeBlocks array in order to access them for localStorage
+$("section").on('click', '.saveBtn', function() {
+    // select 'this' (the savebutton) and give it its id attribute
+    var saveBtnId = $(this).attr("id");
+
+    // select 'this', then go up to parent (the row) and give it its id attribute
+    var timeBlockRow = $(this)
+    .parent()
+    .attr("id");
+
+    // select 'this, go up to parent, find the class 'hour' then take its text and trim it
+    var timeBlockHour = $(this)
+    .parent()
+    .find(".hour")
+    .text()
+    .trim();
+
+    // select 'this, go up to parent, find the class 'col-10' then take its value (the user text input)
+    var timeBlockText = $(this)
+    .parent()
+    .find(".col-10")
+    .val();
+
+    console.log(saveBtnId, timeBlockRow, timeBlockHour, timeBlockText);
+
+    // push the needed variables into the data array inside timeBlocks
+    timeBlocks.data.push({
+        row: timeBlockRow,
+        hour: timeBlockHour,
+        event: timeBlockText
+    })
+
+    saveTimeBlocks();
+});
+
+loadTimeBlocks();
