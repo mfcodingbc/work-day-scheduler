b5e53cfdee77e2d11d35f86929ec1c097484dc73
Mitchell Frey <mfcodingbc@gmail.com>
2022-03-13 00:37:42 -0500

    buttons redone in HTML, stuck on dynamic id selector function for specific saves

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,27 @@
+// this variable stores an array with every hour and event
+var timeBlocks = {};
+
+// this variable selects all timeblocks(rows)
+var timeBlocksSections = document.querySelectorAll('.row');
+    for (var i = 0; i < timeBlocksSections.length; i++)
+        timeBlocksSections[i].id = 'row-' + i;
+
+// create and set ids for all hours
+var timeBlocksHourEl = document.querySelectorAll('.hour');
+    for (var i = 0; i < timeBlocksHourEl.length; i++)
+        timeBlocksHourEl[i].id = 'hour-' + i;
+
+// create and set ids for all events per hour
+var timeBlocksEventEl = document.querySelectorAll('.col-10');
+    for (var i = 0; i < timeBlocksEventEl.length; i++)
+        timeBlocksEventEl[i].id = 'event-' + i;
+
+// create and set ids for all save buttons
+var timeBlocksSaveEl = document.querySelectorAll('.saveBtn');
+    for (var i = 0; i < timeBlocksSaveEl.length; i++)
+        timeBlocksSaveEl[i].id = 'saveBtn-' + i;
+
+// adding current times with Moment.js
 var currentDayEl = document.querySelector("#currentDay");
 
 // current Day (no specified time!) used in header
@@ -9,11 +33,8 @@ currentDayEl.textContent = currentDay;
 var currentTime = moment().format('LT');
 console.log(currentTime);
 
-// var timeBlocks = {};
+var saveTimeBlocks = function() {
+    localStorage.setItem("timeblocks", JSON.stringify(timeBlocks));
+};
 
-// if(!timeBlocks) {
-//     timeBlocks = {
-//         hour: [],
-//         event: [],
-//     };
-// };
+// Create function that assigns ids based on position in timeBlocks array in order to access them for localStorage
